1694f8da9e9e1f77053ae30cdbc48710317a51cd
SurendarSiva <surendarkrt@gmail.com>
2023-09-16 13:44:30 +0530

    first commit

---
+++ starter/script.js
@@ -0,0 +1,20 @@
+//PRINTS OR SHOWS THE ERRORS IN THE PROGRAM
+'use strict';
+
+
+// let hasDrivingLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDrivingLicens = true;
+// if (hasDrivingLicense) console.log(`I can drive`);
+
+// let interface = 12;
+// let public = 15;
+
+
+//-----------------CREATING THE FIRST FUNCTION
+function logger() {
+    console.log(`I am trying to become FSDev!!!!!`);
+}
+
+logger();
